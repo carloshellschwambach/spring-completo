4969082ea8450f1e4569d62a571d5f6f698b3877
Carlos Henrique Hell Schwambach <carlos.schwambach13@gmail.com>
2024-11-02 01:58:18 -0300

    :tada: First commit

---
+++ spring-completo/src/main/java/io/github/carloshellschwambach/spring_completo/SpringCompletoApplication.java
@@ -0,0 +1,13 @@
+package io.github.carloshellschwambach.spring_completo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCompletoApplication.class, args);
+	}
+
+}
---
+++ spring-completo/src/test/java/io/github/carloshellschwambach/spring_completo/SpringCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carloshellschwambach.spring_completo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
